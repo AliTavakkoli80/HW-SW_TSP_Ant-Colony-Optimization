d8d38fa4aab0dcd552745d0fba322a24bd4b22af
seyed ali <ali.tolyat@yahoo.com>
2022-06-30 15:18:32 +0430

    change Data from a cpp class to hardware module.

--- HW-SW_TSP_Ant-Colony-Optimization/TSP_Ant-Colony-Optimization/TSP_Ant-Colony-Optimization.cpp
+++ HW-SW_TSP_Ant-Colony-Optimization/TSP_Ant-Colony-Optimization/TSP_Ant-Colony-Optimization.cpp
@@ -11,46 +11,102 @@
 using namespace std;
  //Hello_world is module name
  SC_MODULE(hardware) {
+     
+     vector<pair<double, double>> cityGraphCoords;               // cities coordinations
+     vector<vector<double>> cost;                                // Cost (distance) for going from i to j
+     vector<vector<double>> visibility;                          // Visibility from city i to j
+     vector<vector<double>> T;                                   // Pheromone intensity cities i and j
+     sc_int<64> N;                                               // Number of cities
+
 	SC_CTOR(hardware) {
+        N = 5;                                                  // Setting number of citeis
 
-		// Nothing in constructor
+        cityGraphCoords.push_back(make_pair(1, 0));             // Setting cities coordinations
+        cityGraphCoords.push_back(make_pair(4, 1));
+        cityGraphCoords.push_back(make_pair(4, 2));
+        cityGraphCoords.push_back(make_pair(1, 3));
+        cityGraphCoords.push_back(make_pair(0, 1.5));
+
+        vector<double> L, M, U;                                 // Putting Zero vectors in cost and T and visibility
+        cost.push_back(L);                                      // to avoiding errors
+        T.push_back(M);
+        visibility.push_back(U);
+
+        // for (int i = 0; i < N; i++)
+        // {
+        //     double x, y;
+        //     // cout << "Enter city coordinates : ";
+        //     // cin >> x >> y;
+        //     // cityCoords.push_back(make_pair(x, y));
+        // }
+
+        for (int i = 0; i < N; i++)                             // Calculating distance between cities
+        {
+            vector<double> C(N + 1);
+            vector<double> t(N + 1);
+            vector<double> l(N + 1);
+            // cout << "co : "
+            //      << "(" << cityGraphCoords[i].first << "," << cityGraphCoords[i].second << ")\n";
+            for (int j = 1; j <= N; j++)
+            {
+                // cin >> V[j];
+                C[j] = sqrt(pow((cityGraphCoords[i].first - cityGraphCoords[j - 1].first), 2) +
+                    pow((cityGraphCoords[i].second - cityGraphCoords[j - 1].second), 2));
+                // cout << "V: "
+                //      << "(" << cityGraphCoords[i].first << "," << cityGraphCoords[i].second << ") ->"
+                //      << "(" << cityGraphCoords[j - 1].first << "," << cityGraphCoords[j - 1].second << ") : " << V[j] << "\n";
+                t[j] = 1.0;
+                if (C[j] != 0)
+                {
+                    l[j] = 1 / C[j];
+                }
+            }
+            cost.push_back(C);
+            T.push_back(t);
+            visibility.push_back(l);
+        }
 	}
-	SC_METHOD (calculate) {
-
-		//Print "Hello World" to the console.
-
-		cout << "Hello World.\n";
-
-		int age;
-
-		cin >> age;
+	double tourDist(vector<int> inputPath)         // Calculating distance to the city where an ant has gone
+    {
+        int len = inputPath.size() - 1;
+        double tourDist = 0.0;
+        for (int i = 0; i < len; i++)
+        {
+            tourDist += cost[inputPath[i]][inputPath[i + 1]];
+        }
+        tourDist += cost[inputPath[len]][inputPath[0]];
+        return tourDist;
 	}
+    void printVector(vector<int> C)             // Printing a vector in route format
+    {
+        for (int i = 0; i < C.size(); i++)
+            cout << C[i] << "->";
+        cout << "\n";
+    }
 };
 
 
  class Data
  {
  public:
-     vector<pair<double, double>> cityGraphCoords;
-     vector<vector<double>> cost; // Cost for going from i to j
-     vector<vector<double>> visibility;
-     vector<vector<double>> T; // Pheromone intensity cities i and j
-     int N;
-     // char s[30];
+     vector<pair<double, double>> cityGraphCoords;               // cities coordinations
+     vector<vector<double>> cost;                                // Cost (distance) for going from i to j
+     vector<vector<double>> visibility;                          // Visibility from city i to j
+     vector<vector<double>> T;                                   // Pheromone intensity cities i and j
+     int N;                                                      // Number of cities
 
-     Data()
-     { 
-         N = 5;
+     Data()                                                      // Main class of program , general things ...
+     {
+         N = 5;                                                  // Setting number of citeis
 
-         cityGraphCoords.push_back(make_pair(1, 0));
+         cityGraphCoords.push_back(make_pair(1, 0));             // Setting cities coordinations
          cityGraphCoords.push_back(make_pair(4, 1));
          cityGraphCoords.push_back(make_pair(4, 2));
          cityGraphCoords.push_back(make_pair(1, 3));
          cityGraphCoords.push_back(make_pair(0, 1.5));
 
-         // exit(0);
-         vector<double> L, M, U;
-         cost.push_back(L);
+         vector<double> L, M, U;                                 // Putting Zero vectors in cost and T and visibility
+         cost.push_back(L);                                      // to avoiding errors
          T.push_back(M);
          visibility.push_back(U);
 
@@ -62,9 +118,9 @@ using namespace std;
          //     // cityCoords.push_back(make_pair(x, y));
          // }
 
-         for (int i = 0; i < N; i++)
+         for (int i = 0; i < N; i++)                             // Calculating distance between cities
          {
-             vector<double> V(N + 1);
+             vector<double> C(N + 1);
              vector<double> t(N + 1);
              vector<double> l(N + 1);
              // cout << "co : "
@@ -72,24 +128,24 @@ using namespace std;
              for (int j = 1; j <= N; j++)
              {
                  // cin >> V[j];
-                 V[j] = sqrt(pow((cityGraphCoords[i].first - cityGraphCoords[j - 1].first), 2) +
+                 C[j] = sqrt(pow((cityGraphCoords[i].first - cityGraphCoords[j - 1].first), 2) +
                      pow((cityGraphCoords[i].second - cityGraphCoords[j - 1].second), 2));
                  // cout << "V: "
                  //      << "(" << cityGraphCoords[i].first << "," << cityGraphCoords[i].second << ") ->"
                  //      << "(" << cityGraphCoords[j - 1].first << "," << cityGraphCoords[j - 1].second << ") : " << V[j] << "\n";
                  t[j] = 1.0;
-                 if (V[j] != 0)
+                 if (C[j] != 0)
                  {
-                     l[j] = 1 / V[j];
+                     l[j] = 1 / C[j];
                  }
              }
-             cost.push_back(V);
+             cost.push_back(C);
              T.push_back(t);
              visibility.push_back(l);
          }
      }
 
-     double tourDist(vector<int> inputPath)
+     double tourDist(vector<int> inputPath)                      // Calculating distance to the city where an ant has gone     
      {
          int len = inputPath.size() - 1;
          double tourDist = 0.0;
@@ -101,7 +157,7 @@ using namespace std;
          return tourDist;
      }
 
-     void printVector(vector<int> C)
+     void printVector(vector<int> C)                             // Printing a vector in route format
      {
          for (int i = 0; i < C.size(); i++)
              cout << C[i] << "->";
@@ -109,40 +165,40 @@ using namespace std;
      }
  };
 
- Data globalData; // global data construct
+ hardware globalData; // global data construct
 
- class Ant
+ class Ant                                                       // Ant implementation
  {
  public:
-     vector<int> trail;
-     set<int, greater<int>> available;
-     double alpha; // importance of the pheromone level T
-     double beta;  // importance of the visibility
+     vector<int> trail;                                          // Routes an ant has gone
+     set<int, greater<int>> available;                           // Cities that are available for ant to go 
+     double alpha;                                               // Importance of the pheromone level T
+     double beta;                                                // Importance of the visibility
      Ant(double a, double b)
      {
          alpha = a;
          beta = b;
-         trail.push_back(1); // always start from the city (1)
+         trail.push_back(1);                                     // Always start from the city (1)
          for (int i = 2; i <= globalData.N; i++)
          {
-             available.insert(i);
+             available.insert(i);                                // All cities are available except city 1
          }
      }
-     void reset()
+     void reset()                                                // Reseting an ant 
      {
          vector<int> tmp;
          tmp.push_back(1);
-         trail = tmp; // reset to nest.
+         trail = tmp;                                            // reset to city 1.
          for (int i = 2; i <= globalData.N; i++)
          {
              available.insert(i);
          }
      }
 
-     void pheroRemain()
+     void pheroRemain()                                          // Remaining event of phermone on routes
      {
          double tourDist = globalData.tourDist(trail);
-         const int constNum = 1;
+         const int constNum = 100;
          double depositAmount = constNum / tourDist;
          int n = trail.size() - 1;
          for (int i = 0; i < n; i++)
@@ -152,7 +208,7 @@ using namespace std;
          globalData.T[trail[n]][trail[0]] += depositAmount;
      }
 
-     vector<int> stop()
+     vector<int> stop()                                          // Stopping an ant from stepping
      {
          pheroRemain();
          vector<int> temp = trail;
@@ -160,10 +216,10 @@ using namespace std;
          return temp;
      }
 
-     void step()
+     void step()                                                 // Stepping an ant from a city to another 
      {
-         int currentCity = trail[trail.size() - 1];
-         double norm = probabilityNorm(currentCity);
+         int currentCity = trail[trail.size() - 1];              // Calculating current city
+         double norm = probabilityNorm(currentCity);             // Callculating sum of all probability of available cities
          double p, gp;
          bool moved = false;
          double highestProb = 0;
@@ -177,14 +233,16 @@ using namespace std;
                  cityHighest = *i;
                  highestProb = p;
              }
-             gp = getRand();
-             if (gp <= p)
-             { // move
-                 moved = true;
-                 trail.push_back(*i);
-                 available.erase(i);
-                 break;
-             }
+             // Probability  a random ant dont go to the highest probability city 
+             //gp = getRand();
+             //if (gp <= p)
+             //{                                           // move
+             //    cout << (gp) << endl;
+             //    moved = true;
+             //    trail.push_back(*i);
+             //    available.erase(*i);
+             //    break;
+             //}
          }
          if (!moved)
          {
@@ -197,17 +255,18 @@ using namespace std;
 
      double getRand()
      {
-         double p = (rand() / (RAND_MAX + 1.0));
+         //srand(time(0));
+         double p = ((double)rand() / ((double)RAND_MAX));
          return p;
      }
-     double moveProbability(int i, int j, double norm)
+     double moveProbability(int i, int j, double norm)                       // Calculating the moving probability of an ant using the formula
      {
          double p = (pow(globalData.T[i][j], alpha)) * (pow(globalData.visibility[i][j], beta));
          p /= norm;
          return p;
      }
 
-     double probabilityNorm(int currentCity)
+     double probabilityNorm(int currentCity)                     // Calculating all available distance probability for the formolla
      {
          int size = available.size();
          double norm = 0.0;
@@ -234,49 +293,50 @@ using namespace std;
          alpha = a;
          beta = b;
          evaporation = e;
-         N = globalData.N;
-         M = 5; // ants
-         for (int i = 0; i < M; i++)
+         N = globalData.N;                           // Number of cities
+         M = 5;                                      // Number of ants
+         for (int i = 0; i < M; i++)                 // Creating ants
          {
              Ant a(alpha, beta);
              ant.push_back(a);
          }
      }
 
-     void run()
+     void run()                                      // Simulation method
      {
-         vector<int> finalPath;
+         vector<int> finalPath;                      // Final path thett is optimum
          double minTourCost, currenTourCost;
-         for (int n = 0; n < 20; n++)
+         for (int i = 0; i < 20; i++)                // Loop For implementing time
          {
-             for (int p = 0; p < (N - 1); p++)
+             for (int j = 0; j < (N - 1); j++)       // Number of total routes an ant must go
              {
-                 for (int i = 0; i < M; i++)
+                 for (int k = 0; k < M; k++)         // All ants take one step
                  {
-                     ant[i].step();
+                     ant[k].step();                  // going from one city to other
                  }
              }
-             for (int i = 0; i < M; i++)
+             for (int k = 0; k < M; k++)             // Stopping all ants who get into first city
              {
-                 vector<int> p = ant[i].stop();
+                 vector<int> antsFinalTrial = ant[k].stop();     // Finding best (MIN) route
                  if (!finalPath.size())
                  {
-                     finalPath = p;
-                     minTourCost = globalData.tourDist(p);
-                     continue;
+                     finalPath = antsFinalTrial;
+                     minTourCost = globalData.tourDist(antsFinalTrial);
                  }
-                 currenTourCost = globalData.tourDist(p);
-                 if (currenTourCost < minTourCost)
-                 {
-                     minTourCost = currenTourCost;
-                     finalPath = p;
+                 else {
+                     currenTourCost = globalData.tourDist(antsFinalTrial);
+                     if (currenTourCost < minTourCost)
+                     {
+                         minTourCost = currenTourCost;
+                         finalPath = antsFinalTrial;
+                     }
                  }
              }
-             for (int i = 1; i <= N; i++)
+             for (int k = 1; k <= N; k++)          // Implementing evapuration
              {
-                 for (int j = 1; j <= N; j++)
+                 for (int l = 1; l <= N; l++)
                  {
-                     globalData.T[i][j] *= evaporation;
+                     globalData.T[k][l] *= evaporation;
                  }
              }
              // for (int m = 0; m < N; m++)
@@ -288,7 +348,7 @@ using namespace std;
              // }
              // cout << "***********************************************************************************************" << endl;
          }
-         printf("%lf\n", minTourCost);
+         cout << minTourCost << endl;
          globalData.printVector(finalPath);
      }
  };
